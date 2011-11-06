07e5b5bd5d83f2e6ebf5b7e620eeadd92b63cac3
Abdelrahman Mahmoud <abdelm.is@gmail.com>
2011-11-06 18:24:01 +0200

    Added score system.

--- index.php
+++ index.php
@@ -12,7 +12,10 @@
 </head>
 <body id="index">
     <canvas id="canvas" width="1000" height="500"></canvas>
-
+	<div id="score">
+		<div style="display: inline;">Score:</div>
+		<div id="count" style="display: inline;">0</div>
+	</div>
     <h1>Use your WASD or arrow keys to move</h1>
 </body>
 </html>
